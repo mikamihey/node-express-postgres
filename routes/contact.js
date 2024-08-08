70f8597d444b8ef40d9b54820cb7a01b3e8403ff
mikamihey <166688422+mikamihey@users.noreply.github.com>
2024-08-08 13:11:14 +0000

    完成

--- routes/contact.js
+++ routes/contact.js
@@ -1,67 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
-const bcrypt = require("bcrypt");
 
 router.get('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
-  res.render('contact', {
-    title: 'contact',
-    isAuth: isAuth,
-  
-  });
-});
-router.post('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
-  const username = req.body.username;
-  const password = req.body.password;
-  const repassword = req.body.repassword;
-
-  knex("users")
-    .where({name: username})
-    .select("*")
-    .then(async function (result) {
-      if (result.length !== 0) {
-        res.render("contact", {
-          title: "contact",
-          errorMessage: ["9時間労働ね"],
-          isAuth: isAuth,
-        })
-      } else if (password === repassword) {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        console.log(hashedPassword);
-        knex("users")
-          .insert({name: username, password: hashedPassword})
-          .then(function () {
-            res.redirect("/");
-          })
-          .catch(function (err) {
-            console.error(err);
-            res.render("contact", {
-              title: "contact",
-              errorMessage: [err.sqlMessage],
-              isAuth: isAuth,
-            });
-          });
-      } else {
-        res.render("contact", {
-          title: "contact",
-          errorMessage: ["何をしている！"],
+  const isAuth = req.isAuthenticated();
+  if (isAuth) {
+    const userId = req.user.id;
+    knex("tasks")
+      .select("*")
+      .where({user_id: userId})
+      .then(function (results) {
+        res.render('contact', {
+          title: 'contact',
+          todos: results,
           isAuth: isAuth,
         });
-      }
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('contact', {
+          title: 'contact',
+          isAuth: isAuth,
+          errorMessage: [err.sqlMessage],
+        });
+      });
+  } else {
+    res.render('contact', {
+      title: 'contact',
+      isAuth: isAuth,
+    });
+  }
+});
+
+router.post('/', function (req, res, next) {
+  const isAuth = req.isAuthenticated();
+  const userId = req.user.id;
+  knex("tasks")
+    .insert({user_id: userId, content: todo})
+    .then(function () {
+      res.redirect('/')
     })
     .catch(function (err) {
       console.error(err);
-      res.render("signup", {
-        title: "Sign up",
-        errorMessage: [err.sqlMessage],
+      res.render('contact', {
+        title: 'Contact',
         isAuth: isAuth,
-       
+        errorMessage: [err.sqlMessage],
+        name: req.user.name,
       });
     });
 });
-
 module.exports = router;
